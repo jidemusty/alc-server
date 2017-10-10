31c3ab5d3485ab5f4ab25a60ef7665b2bc0971b2
jidemusty <jidemustapha2013@gmail.com>
2017-10-10 08:00:26 +0100

    setup integration tests with supertest

--- test/student.js
+++ test/student.js
@@ -1,44 +1,29 @@
 import 'babel-polyfill';
 import mongoose from 'mongoose';
 import chai from 'chai';
-import chaiHttp from 'chai-http';
+import superTest from 'supertest';
+// import chaiHttp from 'chai-http';
 
 import server from '../start';
 import Student from '../models/Student';
 
-chai.use(chaiHttp);
+// chai.use(chaiHttp);
 
 let should = chai.should();
+let expect = chai.expect;
 
 describe('/GET students', () => {
-    it('it should get all students', (done) => {
-        chai.request(server)
+    it('returns an array of students', (done) => {
+        superTest(server)
             .get('/api/students')
+            .expect('Content-Type', 'application/json')
+            .expect(200)
             .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('array');
-                res.body.length.should.be.eql(1);
-            done();
+                expect(res.status).to.equal(200);
+                expect(res.body).to.be.an('array');
+                expect(res.body.length).to.equal(2);
+                done(); 
             });
-    });
-});
 
-
-describe('POST /student', () => {
-    it('should return the created student successfully', (done) => {
-      chai.request(server)
-        .post('/api/student')
-        .send({
-          name: 'test name',
-          age: '12',
-          sex: 'male',
-          email: 'test@test.com',
-          department: 'Test department'
-        })
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.department.should.be.eql('Test department');
-        done();
-        });
     });
 });
