41319e9a6fd02dfb4947c10a6163a7e5d0453fd8
jidemusty <jidemustapha2013@gmail.com>
2017-10-10 13:49:43 +0100

    revamped config vars

--- config/environments/production.js
+++ config/environments/production.js
@@ -2,4 +2,4 @@ export default {
     env: 'production',
     db: 'mongodb://jidemusty:jidemustapha@ds115045.mlab.com:15045/alc-server',
     port: process.env.PORT || 4000
-  };
+};
--- start.js
+++ start.js
@@ -5,14 +5,17 @@ import config from './config/environments';
 require('dotenv').config({ path: '.env' });
 
 // connect to our database and handle any bad connections
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(config.default.db);
+
 // tell mongoose to use ES6 promises
 mongoose.Promise = global.Promise
+
 mongoose.connection.on('error', (err) => {
     console.error(`${err.message}`);
 });
+
 mongoose.connection.on('connected', () => {
-    console.log(`Connected to database: ${process.env.DATABASE}`);
+    console.log(`Connected to database: ${config.default.db}`);
 });
 
 if (config.env === 'development') {
@@ -22,7 +25,7 @@ if (config.env === 'development') {
 // start our app!
 import app from './config/app';
 
-app.set('port', process.env.PORT || 7777);
+app.set('port', config.default.port);
 const server = app.listen(app.get('port'), () => {
     console.log(`Express running -> PORT ${server.address().port}`);
 });
